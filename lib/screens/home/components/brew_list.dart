5d6aee5b4bfc661ada1ba01c189adca3279d7762
Sixtus Miracle <67114923+SixtusMiracle@users.noreply.github.com>
2022-03-21 16:02:09 +0100

    listing brew data on the ui

--- lib/screens/home/components/brew_list.dart
+++ lib/screens/home/components/brew_list.dart
@@ -2,6 +2,8 @@ import 'package:brew/models/brew.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'brew_tile.dart';
+
 class BrewList extends StatefulWidget {
   const BrewList({Key? key}) : super(key: key);
 
@@ -13,12 +15,12 @@ class _BrewListState extends State<BrewList> {
   @override
   Widget build(BuildContext context) {
     final brews = Provider.of<List<Brew>?>(context);
-    brews!.forEach((brew) {
-      print(brew.name);
-      print(brew.sugars);
-      print(brew.strength);
-    });
 
-    return Container();
+    return ListView.builder(
+      itemCount: brews!.length,
+      itemBuilder: (BuildContext context, int index) {
+        return BrewTile(brew: brews[index]);
+      },
+    );
   }
 }
